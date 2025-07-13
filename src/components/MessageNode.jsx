52c204f7195d04c2949bb842f46686f463641810
AbhishekSChauhan <abhishek.uietpuchd@gmail.com>
2025-07-13 21:46:03 +0530

    fix message node

--- src/components/MessageNode.jsx
+++ src/components/MessageNode.jsx
@@ -1,29 +1,39 @@
-import { memo } from 'react';
-import { Handle, Position, useReactFlow } from '@xyflow/react';
+import { memo } from "react";
+import { Handle, Position, useReactFlow } from "@xyflow/react";
 
-const MessageNode = memo(({id,data}) => {
-
+const MessageNode = memo(({ id, data }) => {
   const { getEdges } = useReactFlow();
 
-  // Only allow 1 outgoing connection per source handle
+  // Allow only 1 outgoing edge
   const isValidSourceConnection = () => {
     const edges = getEdges();
-    const hasOutgoing = edges.some((e) => e.source === id);
-    return !hasOutgoing; 
+    return !edges.some((e) => e.source === id);
   };
 
-  
-
   return (
-    <div className="p-2 bg-white border rounded shadow text-[14px]">
-      <Handle type="target" position={Position.Left} />
-      <div>{data.label}</div>
+    <div className="max-w-36 rounded-lg border text-[14px] border-gray-300 shadow-md bg-white">
+      <div className="flex items-center justify-between p-1 rounded-t-lg bg-green-100 border-b border-gray-200">
+        <div className="flex items-center space-x-1 font-medium">
+          <span>Send Message</span>
+        </div>
+        
+      </div>
+
+      <div className="p-2 text-gray-700 text-sm">
+        {data?.label}
+      </div>
+
+      <Handle
+        type="target"
+        position={Position.Left}
+      />
       <Handle
         type="source"
         position={Position.Right}
         isValidConnection={isValidSourceConnection}
       />
     </div>
-  )});
+  );
+});
 
 export default MessageNode;
